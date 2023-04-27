f0ce5cb51c6d54eb8910e12acb3bee1e33191a5b
Pietro Maciel <pietroomv150206@gmail.com>
2023-04-27 20:16:54 -0300

    .

--- src/controller/view/GetPersonIdView.ts
+++ src/controller/view/GetPersonIdView.ts
@@ -5,10 +5,11 @@ import { GetPersonIdValidation } from "../../validations/GetPersonIdValidation";
 export class GetPersonIdView {
     async personGetById(req: Request, res: Response) {
         const person = new GetPersonId();
-        const getPersonBody = await person.getPersonId(Number(req.params.id))
+        const id = Number(req.params.id)
+        const getPersonBody = await person.getPersonId(id)
         const validation = new GetPersonIdValidation
         if ( validation.validatePersonToGetById(getPersonBody) == false) {
-            res.status(404).json({message: ' Error on getting person by id. '})
+            res.status(404).json({message: `Error on getting person by id: the person with id ${id} doesn't exists. `})
         }
         res.json({message: getPersonBody}) // nao é preciso alterar o http status caso a resposta seja sucesso.
     }
