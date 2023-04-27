14c937429f024d5dae4b530e9cc2d58f9811887e
Pietro Maciel <pietroomv150206@gmail.com>
2023-04-27 20:33:29 -0300

    .

--- src/routes.ts
+++ src/routes.ts
@@ -4,11 +4,39 @@ import { PostPersonView } from './controller/view/PostPersonView'
 import { PutPersonView } from './controller/view/PutPersonView'
 import { GetPersonView } from './controller/view/GetPersonView'
 import { DeletePersonView } from './controller/view/DeletePersonView'
-import { PutPerson } from './controller/PutPerson'
+
 const routes = Router()
 
 routes.get('/', (req: Request, res: Response) => {
-    return res.json({ message: 'Hello World' })
+    return res.send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Person List API</title>
+          </head>
+          <body>
+            <h1>Welcome to the Person List API</h1>
+            <p>This API allows you to view a list of people and access individual person information.</p>
+            <button onclick="window.location.href='/person'">View Person List</button>
+            <button onclick="showInput()">View Person by ID</button>
+            <div id="inputContainer" style="display:none">
+              <input type="text" id="personIdInput" placeholder="Enter person ID" />
+              <button onclick="submitPersonId()">Send</button>
+            </div>
+            <script>
+              function showInput() {
+                document.getElementById("inputContainer").style.display = "block";
+              }
+              function submitPersonId() {
+                const id = document.getElementById("personIdInput").value;
+                if (id) {
+                  window.location.href = '/person/' + id;
+                }
+              }
+            </script>
+          </body>
+        </html>
+    `)
 })
 
 routes.get('/person', (req, res) => new GetPersonView().personGet(req, res))
